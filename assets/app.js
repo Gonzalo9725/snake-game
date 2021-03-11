a009e5067a5c61a7afeb064cc71044d56465d58e
Gonzalo9725 <varasgonzalo97@gmail.com>
2021-03-10 23:01:27 -0300

    add keydown event listener to start moving the snake

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,9 @@
 let canvasElement = document.getElementById("canvasGame");
 let ctx = canvasElement.getContext("2d");
 
+let snakePosX = 0;
+let snakePosY = 0;
+
 const drawGrid = (context) => {
   for (let x = 15; x < 600; x += 15) {
     context.beginPath();
@@ -27,4 +30,27 @@ const drawSnake = (context, posX, posY) => {
 };
 
 drawGrid(ctx);
-drawSnake(ctx, 0, 0);
+drawSnake(ctx, snakePosX, snakePosY);
+
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowUp":
+      snakePosY -= 15;
+      break;
+    case "ArrowDown":
+      snakePosY += 15;
+      break;
+    case "ArrowLeft":
+      snakePosX -= 15;
+      break;
+    case "ArrowRight":
+      snakePosX += 15;
+      break;
+    default:
+      return;
+  }
+
+  ctx.clearRect(0, 0, 600, 600);
+  drawGrid(ctx);
+  drawSnake(ctx, snakePosX, snakePosY);
+});
